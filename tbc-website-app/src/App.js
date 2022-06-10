26a410bed906b41139bfcb1458665438c8b37bac
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-06-10 11:27:46 -0400

    add App.css, App.js, edit html, and more

---
+++ tbc-website-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='all'>
+        <div className="green-box"> 
+            <h1 id="title-header">TBC Guide Services</h1>
+        </div>
+
+        <div className='buttons'>
+            <button id="button-1" type="button">About Me</button>
+            <button id="button-2" type="button">Bookings</button>
+            <button id="button-3" type="button">Location</button>
+            <button id="button-4" type="button">Photos & Videos</button>
+            <button id="button-5" type="button">Testimonials</button>
+        </div>
+    </div>
+    
+
+  );
+}
+
+export default App;
