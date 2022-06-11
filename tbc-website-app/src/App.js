6423b69c6e723b6a2620da32b1e8ffb1174d3f4b
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-06-11 12:17:08 -0400

    add photos and video components

--- tbc-website-app/src/App.js
+++ tbc-website-app/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import './index.css';
+import Vids from './components/Vids'; 
+import Photos from './components/Photos'; 
+
 
 function App() {
   return (
@@ -6,13 +10,23 @@ function App() {
         <div className="green-box"> 
             <h1 id="title-header">TBC Guide Services</h1>
         </div>
-
+        <Vids></Vids>
         <div className='buttons'>
-            <button id="button-1" type="button">About Me</button>
+            <button id="button-1" type="button">About</button>
             <button id="button-2" type="button">Bookings</button>
             <button id="button-3" type="button">Location</button>
             <button id="button-4" type="button">Photos & Videos</button>
             <button id="button-5" type="button">Testimonials</button>
+        </div>
+        <div className='welcome'>
+            <h4>WELCOME TO THE PREMIER</h4>
+            <h4>FISHING EXPERIENCE IN THE UPPER VALLEY</h4>
+        </div>
+        <div className='grey-box' >
+            <h1 id="address-and-number">4 Hunter Lane Hanover, NH 03755 • (828)-246-5616</h1>
+        </div>
+        <div className='photo-box-home'>
+           <Photos></Photos>
         </div>
     </div>
     
